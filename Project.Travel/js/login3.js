284364eef6b7b4872d5021880e152a768558cb16
admin <admin@CSGLANBK-281.sg.cmc.com.vn>
2019-12-28 23:11:06 +0700

    ''

--- Project.Travel/js/login3.js
+++ Project.Travel/js/login3.js
@@ -1,5 +1,5 @@
 let getSignUp =document.getElementById('fail-button')
 getSignUp.addEventListener('click',function(item){
     item.preventDefault()
-    window.location.href="file:///C:/Users/admin/Desktop/Javascript%20Mindx/Fundamentals/Project.Travel/signin.html"
+    window.location.href="signin.html"
 })
--- Project.Travel/login1.js
+++ Project.Travel/login1.js
@@ -2,5 +2,5 @@ let getok=document.getElementById('success-button')
 getok.addEventListener('click',function(item){
     item.preventDefault()
     console.log(100)
-    window.location.href="file:///C:/Users/admin/Desktop/Javascript%20Mindx/Fundamentals/Project.Travel/index-matching.html#team"
+    window.location.href="index-matching.html"
 })
