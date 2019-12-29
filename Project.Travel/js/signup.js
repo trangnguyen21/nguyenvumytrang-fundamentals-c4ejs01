1faac1f114efa9509e900c71d4f09a9990b71bae
admin <admin@CSGLANBK-281.sg.cmc.com.vn>
2019-12-29 09:28:01 +0700

    ''

--- Project.Travel/js/signup.js
+++ Project.Travel/js/signup.js
@@ -1,104 +1,107 @@
-let getSignUp =document.getElementById('signup-button')
-getSignUp.addEventListener('click',function(item){
+let getSignUp = document.getElementById('signup-button')
+getSignUp.addEventListener('click', function (item) {
     item.preventDefault();
     /*---------------------------------------------*/
-    let user ={
-        Name :document.getElementById('name-input').value,
-        email :document.getElementById('email-input').value,
-        UserName :document.getElementById('username-input').value,
-        password :document.getElementById('password-input').value,
-        confilmpassword :document.getElementById('confilm').value
+    let user = {
+        Name: document.getElementById('name-input').value,
+        email: document.getElementById('email-input').value,
+        UserName: document.getElementById('username-input').value,
+        password: document.getElementById('password-input').value,
+        confilmpassword: document.getElementById('confilm').value
     }
     /*---------CHECK-EMAIL--------*/
-    const mail =document.getElementById('email-input')
-        function ValidateEmail(inputText){
-            var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-            if(inputText.value.match(mailformat))
-            {
-                return true;
-            }
-            else{
-                mail.value=''
-                return false;
-            }
-    } 
-    const checks=ValidateEmail(mail)
+    const mail = document.getElementById('email-input')
+    function ValidateEmail(inputText) {
+        var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        if (inputText.value.match(mailformat)) {
+            return true;
+        }
+        else {
+            mail.value = ''
+            return false;
+        }
+    }
+    const checks = ValidateEmail(mail)
 
     /*---------END-CHECK EMAIL---*/
 
     /*---------CHECK-INPUT--------*/
-    function Check(user){
-        if(user.Name =="" || user.email =="" || user.UserName=="" || user.password==""|| user.confilmpassword==""){
-            return 0; 
-        }else{
+    function Check(user) {
+        if (user.Name == "" || user.email == "" || user.UserName == "" || user.password == "" || user.confilmpassword == "") {
+            return 0;
+        } else {
             return 1;
         }
     }
     /*-------------------------------*/
 
-    const check=Check(user)
-    if(check ==1 &&checks){
+    const check = Check(user)
+    if (check == 1 && checks) {
         /*-------SOSANH PASSWORD VS CONFILM PASSWORD-----*/
-        if(user.password ==user.confilmpassword){
+        if (user.password == user.confilmpassword) {
             /*------------GET DATA SOSANH INPUT-EMAIL ----------*/
-            async function data(user){
-                let request =await fetch('http://5dfe28f303f98e0014e92871.mockapi.io/api/v1/user')
-                let data =await request.json()
-                let find =data.filter(function(item){
-                    return item.email ==user.email
+            async function data(user) {
+                let request = await fetch('http://5dfe28f303f98e0014e92871.mockapi.io/api/v1/user')
+                let data = await request.json()
+                let find = data.filter(function (item) {
+                    return item.email == user.email
                 })
-                if(find.length !=0){
+                if (find.length != 0) {
                     /* email used*/
-                    window.location.href="login3.html"
-                }else {return 100}
+                    window.location.href = "login3.html"
+                } else { return 100 }
             }
             /*---------------------------------------------------*/
-            
-            async function postdata(user){
-                const check =await data(user)
-                if(check ==100){
-                const setting ={
-                    method:"POST",
-                    body :JSON.stringify(user),
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json',
+
+            async function postdata(user) {
+                const check = await data(user)
+                if (check == 100) {
+                    const setting = {
+                        method: "POST",
+                        body: JSON.stringify(user),
+                        headers: {
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json',
+                        }
                     }
-                }
-                /*---------------POST DATA ->API--------------*/
-                const response =await fetch('http://5dfe28f303f98e0014e92871.mockapi.io/api/v1/user',setting)
-                if (!response.ok) throw Error(response.message);
-                
-                try {
-                    const data = await response.json();
-                    return data;
-                }
-                catch (err) {
-                    throw err;
-                }
-                
-                }else {
+                    /*---------------POST DATA ->API--------------*/
+                    const response = await fetch('http://5dfe28f303f98e0014e92871.mockapi.io/api/v1/user', setting)
+                    if (!response.ok) throw Error(response.message);
+
+                    try {
+                        const data = await response.json();
+                        return data;
+                    }
+                    catch (err) {
+                        throw err;
+                    }
+
+                } else {
                     return 0;
                 }
             }
 
             /*-----------SIGNUP SUCCESS------------------*/
 
-            async function getpost(){
-                let resutl =await postdata(user)
-                if(resutl !=0){
-                    window.location.href="login1.html"
+            async function getpost() {
+                let resutl = await postdata(user)
+                if (resutl != 0) {
+                    window.location.href = "login1.html"
                 }
             }
             data(user)
             getpost()
 
             /* ---case password != confilmlpassword------*/
-            }else{
-                let password=document.getElementById('confilm')
-                password.value=""
-            }
-        }else{
-            console.log("hehe")
+        } else {
+            let password = document.getElementById('confilm')
+            password.value = "";
+            let erro =document.getElementById('message-erro')
+            erro.innerHTML=`<p id='mg-erro'>Mật khẩu nhập lại bị sai!<p>`
+
+
         }
+    } else {
+        console.log("hehe")
+    }
 })
